37d1959ad5cf5293c9a5aed84018d08b9016655c
xufangjie <xufangjie2008@163.com>
2022-02-08 16:27:47 +0800

    init project

---
+++ demo-client/src/main/java/com/mingshashan/learn/sofa/ClientApplication.java
@@ -0,0 +1,11 @@
+package com.mingshashan.learn.sofa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
---
+++ demo-server/src/main/java/com/mingshashan/learn/sofa/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.mingshashan.learn.sofa;
+
+import org.apache.catalina.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
